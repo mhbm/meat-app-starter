608e9cabd8669133e7a44f8d5d936afdf79e5c9a
Mateus Macedo <mhbassomacedo@gmail.com>
2019-11-06 16:44:24 -0300

    Criado o envio da compra por POST. No arquivo db.json foi adicionado duas compras com sucesso
    Signed-off-by: Mateus Macedo <mhbassomacedo@gmail.com>

--- src/app/order/order.component.ts
+++ src/app/order/order.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { RadioOption } from '../shared/radio/radio-option.model';
 import { OrderService } from './order.service';
 import { CartItem } from '../restaurant-detail/shopping-cart/cart-item.model';
+import { Order, OrderItem } from './order.model';
 
 @Component({
   selector: 'mt-order',
@@ -12,9 +13,9 @@ export class OrderComponent implements OnInit {
   delivery: number = 8 //valor fixo
 
   paymentOption: RadioOption[] = [
-    { "label" : "Dinheiro", "value" : "MON"},
-    { "label" : "Cartão de Débito", "value" : "DEB"},
-    { "label" : "Cartão de Refeição", "value" : "REF"}
+    { "label": "Dinheiro", "value": "MON" },
+    { "label": "Cartão de Débito", "value": "DEB" },
+    { "label": "Cartão de Refeição", "value": "REF" }
   ]
 
   constructor(private orderService: OrderService) { }
@@ -22,7 +23,7 @@ export class OrderComponent implements OnInit {
   ngOnInit() {
   }
 
-  itemsValue():number {
+  itemsValue(): number {
     return this.orderService.itemsValue()
   }
 
@@ -40,6 +41,17 @@ export class OrderComponent implements OnInit {
 
   remove(item: CartItem) {
     this.orderService.remove(item)
+  }
+
+  checkOrder(order: Order) {
+    order.orderItems = this.cartItems()
+      .map((item: CartItem) => new OrderItem(item.quantity, item.menuItem.id))
+
+    this.orderService.checkOrder(order).subscribe( (orderId: string) => {
+      console.log(`Compra concluida ${orderId}`)
+      this.orderService.clear()
+    })
+    console.log(order)
   }
 
 }
