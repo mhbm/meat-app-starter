7ed05f1298afd9cfe47500a9c2dbe4f6de1dd4a5
Mateus Macedo <mhbassomacedo@gmail.com>
2019-11-06 14:20:55 -0300

    Criando Componente Usando ControlValueAccessor - radio
    Signed-off-by: Mateus Macedo <mhbassomacedo@gmail.com>

--- src/app/order/order.component.ts
+++ src/app/order/order.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { RadioOption } from 'app/shared/radio/radio-option.model';
 
 @Component({
   selector: 'mt-order',
@@ -6,9 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrderComponent implements OnInit {
 
+  paymentOption: RadioOption[] = [
+    { "label" : "Dinheiro", "value" : "MON"},
+    { "label" : "Cartão de Débito", "value" : "DEB"},
+    { "label" : "Cartão de Refeição", "value" : "REF"}
+  ]
+
   constructor() { }
 
   ngOnInit() {
   }
+
+
 
 }
