5af276c14e76507f7434b37cf6ad4e94154ddd7a
Mateus Macedo <mhbassomacedo@gmail.com>
2019-11-06 14:47:23 -0300

    Alterado o componente para ValueAccessor
    Signed-off-by: Mateus Macedo <mhbassomacedo@gmail.com>

--- src/app/shared/radio/radio.component.ts
+++ src/app/shared/radio/radio.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit, Input} from '@angular/core';
+import { Component, OnInit, Input, forwardRef} from '@angular/core';
+import { NG_VALUE_ACCESSOR, ControlValueAccessor} from '@angular/forms'
 import { RadioOption } from './radio-option.model';
 
+
+//Registrar o componente como um ValueAcessor que fica disponivel para as diretivas 
 @Component({
   selector: 'mt-radio',
-  templateUrl: './radio.component.html'
+  templateUrl: './radio.component.html',
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(()=> RadioComponent),
+      multi : true
+    }
+  ]
 })
-export class RadioComponent implements OnInit {
+export class RadioComponent implements OnInit, ControlValueAccessor {
 
   @Input() options: RadioOption[]
 
   value : any
+
+  onChange : any
 
   constructor() { }
 
@@ -18,6 +30,28 @@ export class RadioComponent implements OnInit {
 
   setValue(value : any) {
     this.value = value;
+    this.onChange(this.value)
   }
+
+  //Chamado pela diretiva quando quer passar um valor
+  writeValue(obj: any) {
+    this.value = obj;
+  }
+
+  //Passa uma função e essa função é sempre chamado quando o valor interno do componente é alterado
+  registerOnChange(fn: any) {
+    this.onChange = fn
+  }
+
+
+  //Quando o usuário entra no componente
+  registerOnTouched(fn: any) {
+
+  }
+
+  setDisabledState(fn: any) {
+
+  }
+
 
 }
