eaf19b8218e01652a1d33d829e987cc9b7ce549a
Mateus Macedo <mhbassomacedo@gmail.com>
2019-11-07 17:23:10 -0300

    Colocando o catch para prevenir queda do backend(servidor). Colocado mensagem quando não é encontrado nenhum item na busca
    Signed-off-by: Mateus Macedo <mhbassomacedo@gmail.com>

--- src/app/restaurants/restaurants.component.ts
+++ src/app/restaurants/restaurants.component.ts
@@ -7,6 +7,9 @@ import { FormBuilder, FormGroup, FormControl } from '@angular/forms';
 import 'rxjs/add/operator/switchMap'
 import 'rxjs/add/operator/debounceTime'
 import 'rxjs/add/operator/distinctUntilChanged'
+import 'rxjs/add/operator/catch'
+import 'rxjs/add/observable/from'
+import { Observable } from 'rxjs/Observable'
 
 @Component({
   selector: 'mt-restaurants',
@@ -45,17 +48,23 @@ export class RestaurantsComponent implements OnInit {
       searchControl: this.searchControl
     })
 
-    this.searchControl.valueChanges.debounceTime(500).distinctUntilChanged().switchMap(  
-          searchTerm => this.restaurantsService.restaurants(searchTerm)
-      ).subscribe(restaurants => this.restaurants = restaurants)
+    this.searchControl.valueChanges
+          .debounceTime(500)
+          .distinctUntilChanged()
+          .switchMap( searchTerm => 
+            this.restaurantsService
+                .restaurants(searchTerm)
+                .catch(error => 
+                  Observable.from([])
+                )
+          ).subscribe(restaurants => this.restaurants = restaurants)
     
     this.restaurantsService.restaurants()
       .subscribe(restaurants => this.restaurants = restaurants)
   }
 
   toggleSearch() {
-    console.log("aaa");
-    console.log(this.searchBarState)
+    
     this.searchBarState = this.searchBarState === 'hidden' ? 'visible' : 'hidden'
   }
 
