52968797a9c202b1195dea096cc28d0407414aae
Mateus Macedo <mhbassomacedo@gmail.com>
2019-11-07 10:29:44 -0300

    Removido o modulo CoreModule e utilizado a lib ModuleWithProviders
    Signed-off-by: Mateus Macedo <mhbassomacedo@gmail.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,19 +10,15 @@ import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
 import { RestaurantsComponent } from './restaurants/restaurants.component';
 import { RestaurantComponent } from './restaurants/restaurant/restaurant.component'
-import {RestaurantsService} from './restaurants/restaurants.service';
-import {ShoppingCartService} from './restaurant-detail/shopping-cart/shopping-cart.service';
+
 import { RestaurantDetailComponent } from './restaurant-detail/restaurant-detail.component';
 import { MenuComponent } from './restaurant-detail/menu/menu.component';
 import { ShoppingCartComponent } from './restaurant-detail/shopping-cart/shopping-cart.component';
 import { MenuItemComponent } from './restaurant-detail/menu-item/menu-item.component';
 import { ReviewsComponent } from './restaurant-detail/reviews/reviews.component';
 
-import { OrderService } from './order/order.service';
-
 import { OrderSummaryComponent } from './order-summary/order-summary.component';
 import { SharedModule } from './shared/shared.module';
-import { CoreModule } from './core/core.module';
 
 
 @NgModule({
@@ -42,8 +38,7 @@ import { CoreModule } from './core/core.module';
   imports: [
     BrowserModule,
     HttpModule,
-    SharedModule,
-    CoreModule,
+    SharedModule.forRoot(),
     RouterModule.forRoot(ROUTES)
   ],
   providers: [{provide: LOCALE_ID, useValue : 'pt-BR'}],
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from "@angular/core";
+import { NgModule, ModuleWithProviders } from "@angular/core";
 import { CommonModule} from "@angular/common"
 import {FormsModule, ReactiveFormsModule} from "@angular/forms"
 
@@ -6,6 +6,9 @@ import {FormsModule, ReactiveFormsModule} from "@angular/forms"
 import{ InputComponent } from "./input/input.component"
 import{ RadioComponent } from "./radio/radio.component"
 import{ RatingComponent } from "./rating/rating.component"
+import { ShoppingCartService } from "app/restaurant-detail/shopping-cart/shopping-cart.service";
+import { RestaurantsService } from "app/restaurants/restaurants.service";
+import { OrderService } from "app/order/order.service";
 
 @NgModule({
     declarations: [
@@ -30,4 +33,10 @@ import{ RatingComponent } from "./rating/rating.component"
 
 export class SharedModule {
 
+    static forRoot(): ModuleWithProviders {
+        return {
+            ngModule: SharedModule,
+            providers: [ShoppingCartService, RestaurantsService, OrderService]
+        }
+    }
 }
