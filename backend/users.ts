e3544d33ebc292d924356b32bad04bc880676fa1
Mateus Macedo <mhbassomacedo@gmail.com>
2019-11-08 10:55:43 -0300

    Criação de autorização de login na pasta backend
    Signed-off-by: Mateus Macedo <mhbassomacedo@gmail.com>

---
+++ backend/users.ts
@@ -0,0 +1,15 @@
+export class User {
+    constructor(public email: String, 
+                public name: String, 
+                private password: String) {
+    }
+
+    matches(another: User) : boolean {
+        return another !== undefined && another.email == this.email && another.password === this.password
+    }
+}
+
+export const users = {
+    "juliana@gmail.com" : new User("juliana@gmail.com", "Juliana", "juliana23"),
+    "amanda@gmail.com" : new User("amanda@gmail.com", "Amanda", "amanda21")
+}
