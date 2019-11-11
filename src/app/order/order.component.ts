3bfd6c77614f11f427843fc152132d27fb889654
Mateus Macedo <mhbassomacedo@gmail.com>
2019-11-11 09:54:31 -0300

    Colocado no form a instancia da classe FormGroup e utilizando o parametro updateOn 'blur' nos campos para a validação
    Signed-off-by: Mateus Macedo <mhbassomacedo@gmail.com>

--- src/app/order/order.component.ts
+++ src/app/order/order.component.ts
@@ -4,7 +4,7 @@ import { OrderService } from './order.service';
 import { CartItem } from '../restaurant-detail/shopping-cart/cart-item.model';
 import { Order, OrderItem } from './order.model';
 
-import {FormGroup, FormBuilder, Validators, AbstractControl} from "@angular/forms"
+import {FormGroup, FormBuilder, FormControl ,Validators, AbstractControl} from "@angular/forms"
 
 import { Router } from "@angular/router"
 
@@ -33,16 +33,18 @@ export class OrderComponent implements OnInit {
   constructor(private orderService: OrderService, private router: Router, private formBuilder: FormBuilder) { }
 
   ngOnInit() {
-    this.orderForm = this.formBuilder.group({
+    this.orderForm = new FormGroup({
       //Propriedades são os inputs
-      name: this.formBuilder.control('', [Validators.required, Validators.minLength(5)]),
+      name: new FormControl('',  {
+         validators: [Validators.required, Validators.minLength(5)]
+      }),
       email: this.formBuilder.control('', [Validators.required, Validators.pattern(this.emailPattern)]),
       emailConfirmation: this.formBuilder.control('', [Validators.required, Validators.pattern(this.emailPattern)]),
       address: this.formBuilder.control('', [Validators.required, Validators.minLength(5)]),
       number : this.formBuilder.control('', [Validators.required, Validators.pattern(this.numberPattern)]),
       optionalAddress : this.formBuilder.control(''),
       paymentOption: this.formBuilder.control('', [Validators.required])
-    }, {validator: OrderComponent.equalsTo})
+    }, {validators: [OrderComponent.equalsTo], updateOn: 'blur'})
   }
 
   static equalsTo(group: AbstractControl): {[key:string] : boolean} {
